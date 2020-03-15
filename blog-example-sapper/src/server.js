5f6c4bf3e64f2269712ba46054128c9e0a43207d
Petherson Moreno <pethermoreno@gmail.com>
2020-03-15 18:59:13 -0300

    feat: changed build export to use on github pages

--- blog-example-sapper/src/server.js
+++ blog-example-sapper/src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		(dev?'':'/svelte-study/blog-example-sapper'),
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
